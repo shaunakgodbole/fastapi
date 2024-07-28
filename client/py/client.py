45b243949a3fdc84ea921526b7323111f0405f87
Shaunak Godbole <shaunak@fireworks.ai>
2024-07-27 23:59:29 -0700

    Some more commits

--- client/py/client.py
+++ client/py/client.py
@@ -1,8 +1,17 @@
+import os
 import requests
+import time
 
 if __name__ == "__main__":
-    server = "http://0.0.0.0:8080/hello"
+    server = os.environ.get("SERVER")
+    port = int(os.environ.get("PORT", 8080))
+    count = int(os.environ.get("NUM_REQUESTS", 1))
+    url = f"http://{server}:{port}/hello"
+    print(url)
     
-    for i in range(1):
-        resp = requests.get(server)
+    for i in range(count):
+        print("--------  ", url)
+        resp = requests.get(url)
         print(resp.json())
+
+    time.sleep(86400 * 7)

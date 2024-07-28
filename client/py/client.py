86228ee9a1e0df69bda666286c4e9dd813b741ca
Shaunak Godbole <shaunak@fireworks.ai>
2024-07-28 12:23:16 -0700

    Add support for middleware

--- client/py/client.py
+++ client/py/client.py
@@ -3,19 +3,44 @@ import logging
 import requests
 import time
 
+
+def get_bool_env_var(var_name, default=False):
+  """
+  Reads a boolean value from an environment variable.
+
+  Args:
+    var_name: The name of the environment variable.
+    default: The default value to return if the variable is not set or cannot be parsed.
+
+  Returns:
+    The boolean value of the environment variable, or the default value if there is an error.
+  """
+
+  try:
+    value = os.environ.get(var_name)
+    if value is None:
+      return default
+    return value.lower() in ['true', '1']
+  except ValueError:
+    return default
+
+
 if __name__ == "__main__":
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s [%(levelname)s] %(message)s",
     )
 
-    server = os.environ.get("SERVER")
-    port = int(os.environ.get("PORT", 8080))
-    count = int(os.environ.get("NUM_REQUESTS", 1))
+    server = os.environ.get("SERVER", "0.0.0.0")
+    port = int(os.environ.get("PORT", 8000))
+    count = int(os.environ.get("NUM_REQUESTS", 100))
+    should_sleep = get_bool_env_var("RUN_IN_SERVER", False)
+
     url = f"http://{server}:{port}/hello"
 
     for i in range(count):
         resp = requests.get(url)
         logging.info(resp.json())
 
-    time.sleep(86400 * 7)
+    if should_sleep:
+        time.sleep(86400 * 7)
--- server/py/server.py
+++ server/py/server.py
@@ -1,9 +1,22 @@
 import os
 
 import fastapi
+from fastapi import Request
 import uvicorn
 
+
 app = fastapi.FastAPI()
+@app.middleware("http")
+async def add_custom_logic_middlewear(request: Request, call_next):
+    if request.url.path == "/hello":
+        print("Executing function before")
+        try:
+            response = await call_next(request)
+            return response
+        except Exception as e:
+            raise e
+        finally:
+            print("Executing function after")
 
 count = 1
 @app.get("/hello")
@@ -11,6 +24,8 @@ async def counter():
     global count
     count = count + 1
     print("The count is: ", count)
+    if count % 5 == 0:
+        raise Exception("This is exceptional!")
     return "hello world {}".format(count)
 
 if __name__ == "__main__":

90f229a13a277117ad420848f9e740bdb8920688
Shaunak Godbole <shaunak@fireworks.ai>
2024-07-28 00:21:02 -0700

    Get a configuration that works

--- client/py/client.py
+++ client/py/client.py
@@ -1,17 +1,21 @@
 import os
+import logging
 import requests
 import time
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s [%(levelname)s] %(message)s",
+    )
+
     server = os.environ.get("SERVER")
     port = int(os.environ.get("PORT", 8080))
     count = int(os.environ.get("NUM_REQUESTS", 1))
     url = f"http://{server}:{port}/hello"
-    print(url)
-    
+
     for i in range(count):
-        print("--------  ", url)
         resp = requests.get(url)
-        print(resp.json())
+        logging.info(resp.json())
 
     time.sleep(86400 * 7)
--- server/py/server.py
+++ server/py/server.py
@@ -15,4 +15,4 @@ async def counter():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, server="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
